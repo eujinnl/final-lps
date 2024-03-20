c6c9c70738becdb9bab0bfb886370a3ca436af12
eujinnl <eujinnleong@gmail.com>
2024-03-20 16:05:47 +0100

    yes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
       Leong Poh Seng Agency
     </h1>
     <h2>At LPS Insurance Agency, we are committed to providing peace of mind and financial security to our clients. With years of experience in the insurance industry, we specialize in offering a wide range of insurance solutions tailored to meet the unique needs of individuals, families, and businesses. Our dedicated team of knowledgeable agents takes the time to understand your specific requirements and guide you through the process of selecting the right coverage options. Whether you're seeking auto, home, life, health, or business insurance, we strive to deliver personalized service and comprehensive protection. At XYZ Insurance Agency, your well-being is our priority, and we're here to ensure you're protected today and prepared for tomorrow.</h2>
+    <div className="w-full h-screen bg-green-300"></div>
     <p style={{backgroundColor:"#D3D3D3"}}>Hi Dad</p>
   </div>
      
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,13 +1,12 @@
 "use client"
 import dynamic from "next/dynamic";
-import GeneratePdf from "../components/GeneratePDF";
+//import GeneratePdf from "../components/GeneratePDF";
 
 
 const MyFormPage = () => {
   return (
     <div>
-      <h1>My Form Page</h1>
-      <GeneratePdf />
+      <h1>Products</h1>
     </div>
   );
 };
